a0ed8d9914b1c4285fee53ef08e409b46e55ebb9
KipyegonB <koechbernard08@gmail.com>
2022-08-30 15:44:06 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        const input=document.getElementById("input#searchByID")
+        event.preventDefault
+    fetch (`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(data=> {
+    const title = document.getElementById("title")
+    const summary = document.getElementsById("summary")
+    title.innerHTML = data.title
+    summary.innerHTML= data.summary
+    })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
